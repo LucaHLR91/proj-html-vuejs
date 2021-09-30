3cb70e2f619b352ab468cef82f629441e77aa351
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-30 15:36:29 +0200

    Header terminated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,14 +8,16 @@
 
             <nav class="primary_nav">
                 <ul>
-                    <li v-for="link in nav" :key="link"><a href="#">{{link}}</a></li>
+                    <li v-for="(link, index) in nav" :key="index"><a :class="(link.current) ? 'active' : null" href="#">{{link.text}}</a></li>
                     <li><button>Make an appointment</button></li>
                 </ul>
             </nav>
         </section>
       <!-- HEADER CATCH PHRASE -->
-        <section id="header_catch_phrase">
-
+        <section id="header_catch_phrase" class="wrapper">
+            <h1>Caring<span>For Life</span></h1>
+            <p>Kind words can be short and easy to speak, but their echoes are truly endless.Avada health focuses on you as if it was our own family</p>
+            <button>Learn More</button>
         </section>
     </header>
 </template>
@@ -25,11 +27,23 @@ export default {
     name: 'Header',
     data() {
         return {
-            nav: [
-                'Home',
-                'About',
-                'Department',
-                'Articles'
+            nav: [ 
+                {
+                    text: 'Home',
+                    current: true,
+                },
+                {
+                    text: 'About',
+                    current: false,
+                },
+                {
+                    text: 'Department',
+                    current: false,
+                },
+                {
+                    text: 'Articles',
+                    current: false,
+                },
             ]
         }
     }
@@ -41,7 +55,7 @@ export default {
 
     header {
         width: 100%;
-        height: 1231px;
+        height: 1110px;
         background-image: url('../assets/img/header-image-homepage.jpg');
         background-size: cover;
         background-repeat: no-repeat;
@@ -62,6 +76,7 @@ export default {
                 display: flex;
                 justify-content: center;
                 align-items: center;
+                margin-right: 20px;
 
                 ul {
                    display: flex;
@@ -70,15 +85,67 @@ export default {
                    list-style: none;
                    
                    li {
-                       margin: 0 10px;
+                       margin: 0 20px;
 
                        a {
                            text-decoration: none;
-                           color: white;
+                           color: $primary_text_color;
+                           font-size: 20px;
+
+                           &.active {
+                                color: $brand_color;
+                            }
+                       }
+
+                       button {
+                           color: $primary_text_color;
+                           text-transform: uppercase;
+                           font-size: 15px;
+                           padding: 10px;
+                           border: none;
+                           border-radius: 5px;
+                           background-color: $brand_color;
                        }
                    }
                 }
             }
+
         }
+
+        #header_catch_phrase {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: flex-start;
+                padding: 130px;
+
+                h1 {
+                    color: $primary_text_color;
+                    font-size: 70px;
+                    font-weight: 300;
+
+                    span {
+                        color: $brand_color;
+                        margin-left: 10px;
+                    }
+                }
+
+                p {
+                    width: 460px;
+                    font-size: 20px;
+                    color: $primary_text_color;
+                    margin: 20px 0;  
+                }    
+                
+                button {
+                    color: $primary_text_color;
+                    text-transform: uppercase;
+                    padding: 10px 30px;
+                    font-size: 15px;
+                    background-color: $brand_color;
+                    border: none;
+                    border-radius: 5px;
+                }
+            }
     }
 </style>
