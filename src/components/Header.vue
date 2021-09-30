ce400d27c64edcea3e32e17d5d5bf8475e1cc4de
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-30 12:54:53 +0200

    Header_nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,38 @@
 <template>
-  <header>
+    <header>
       <!-- HEADER NAV -->
-      <section id="header_nav">
+        <section id="header_nav">
+            <div id="header_logo">
+                <img src="../assets/img/medical_logo_1x_light.png" alt="Logo">
+            </div>
 
-      </section>
+            <nav class="primary_nav">
+                <ul>
+                    <li v-for="link in nav" :key="link"><a href="#">{{link}}</a></li>
+                    <li><button>Make an appointment</button></li>
+                </ul>
+            </nav>
+        </section>
       <!-- HEADER CATCH PHRASE -->
-      <section id="header_catch_phrase">
+        <section id="header_catch_phrase">
 
-      </section>
-  </header>
+        </section>
+    </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data() {
+        return {
+            nav: [
+                'Home',
+                'About',
+                'Department',
+                'Articles'
+            ]
+        }
+    }
 }
 </script>
 
@@ -26,5 +45,40 @@ export default {
         background-image: url('../assets/img/header-image-homepage.jpg');
         background-size: cover;
         background-repeat: no-repeat;
+
+        #header_nav {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 20px;
+
+            #header_logo {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .primary_nav {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                ul {
+                   display: flex;
+                   justify-content: center;
+                   align-items: center;
+                   list-style: none;
+                   
+                   li {
+                       margin: 0 10px;
+
+                       a {
+                           text-decoration: none;
+                           color: white;
+                       }
+                   }
+                }
+            }
+        }
     }
 </style>
