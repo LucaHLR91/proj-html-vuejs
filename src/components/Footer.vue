7fbc8cb56f9d302de5cc64fa029b043ef789d6fe
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-01 10:02:21 +0200

    Footer copyright

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,23 @@
 <template>
   <footer>
+      <section class="footer_top">
 
+      </section>
+
+      <section class="footer_bottom">
+        <div class="copyright">
+          <p>© Copiryght 2012-2020</p>
+          <p>Avada Theme by <strong>Theme Fusion</strong></p>
+          <p>All Rights Reserved</p>
+          <p>Powered by <strong>Wordpress</strong></p>
+        </div>
+
+        <div class="footer_help">
+          <div class="support">
+            
+          </div>
+        </div>
+      </section>
   </footer>
 </template>
 
@@ -12,4 +29,34 @@ export default {
 
 <style lang="scss">
 @import '../style/General.scss';
+
+footer {
+  width: 100%;
+  height: 500px;
+  background-color: black;
+
+  .footer_bottom {
+    width: 100%;
+    height: 10%;
+    background-color: #f1efef;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .copyright {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      height: 100%;
+      color: $secondary_text_color;
+      margin-left: 15px;
+
+      p {
+        margin: 0 5px;
+        padding: 0 5px;
+        border-right: 1px solid $secondary_text_color;
+      }
+    }
+  }
+}
 </style>
